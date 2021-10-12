1d5839fd73338a1050251439d39bf7f0c5292076
Max Vologin <vologmax@yandex.ru>
2021-10-12 20:03:50 +0300

    Task 5

--- L1_T5.py
+++ L1_T5.py
@@ -3,12 +3,10 @@ cost = int(input("Введите издержки фирмы: "));
 
 if billing > cost:
     rent = billing / (billing - cost) * 100
+    workN = int(input("Введите количество сотрудников: "));
+    profitNWork = (billing - cost) / workN
     print("Вы сработали в прибыль. Рентабельность выручки:", round(rent, 2), "%");
 elif billing == cost:
     print("Вы сработали в ноль.");
 else:
     print("Вы сработали в убыток");
-
-workN = int(input("Введите количество сотрудников: "));
-profitNWork = (billing - cost) / workN
-print("Прибыль фирмы в расчете на одного сотрудника:", round(profitNWork,2), "руб.");
